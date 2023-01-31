267ec592b74cb39e04156d00cf99de948a6c8d96
NicksFed <nick.guzy76@gmail.com>
2023-01-31 13:35:35 -0800

    refactor checkType function, whether lengths are not a triangle test pass

--- src/__tests__/triangle.tests.js
+++ src/__tests__/triangle.tests.js
@@ -12,7 +12,11 @@ test('should correctly determine whether three lengths are not a triangle', () =
   const notTriangle = new Triangle(3,9,22);
   expect(notTriangle.checkType()).toEqual("not a triangle");
 });
-
+test('should correctly determine whether three lengths make a scalene tiangle', () =>
+{
+  const scalTriangle = new Triangle(4,5,7);
+  expect(scalTriangle.checkType()).toEqual("scalene triangle");
+});
 
 });
 
