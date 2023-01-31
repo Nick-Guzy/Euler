005ff54f538c8083528db2b3d2e4e3818065632f
NicksFed <nick.guzy76@gmail.com>
2023-01-31 10:15:35 -0800

    tested naturalNumber constructor, good fail

--- src/number.js
+++ src/number.js
@@ -1,8 +1,3 @@
-// export default function Triangle(side1, side2, side3) {
-//   this.side1 = side1;
-//   this.side2 = side2;
-//   this.side3 = side3;
-// }
 // Triangle.prototype.checkType = function() {
 //   return "not a triangle";
 // };
@@ -17,3 +12,7 @@
 //     }
 // };
 
+export default function NaturalNumber(value) {
+    // this.value = value;
+
+}
