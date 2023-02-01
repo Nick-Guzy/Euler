91472b45d27380eb9d1125cdd5d56481c4e696ee
NicksFed <nick.guzy76@gmail.com>
2023-01-31 16:22:18 -0800

    added rectangle feature and used arrow notation for triangle.js

--- src/__tests__/triangle.tests.js
+++ src/__tests__/triangle.tests.js
@@ -1,30 +1,28 @@
-import { TestScheduler } from 'jest';
 import Triangle from '../triangle';
 
 describe('Triangle', () => {
   test('should correctly create a triangle with three lengths', () => {
-    const triangle = new Triangle(2,4,5);
+    const triangle = new Triangle(2, 4, 5);
     expect(triangle.side1).toEqual(2);
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).toEqual(5);
-});
-test('should correctly determine whether three lengths are not a triangle', () => {
-  const notTriangle = new Triangle(3,9,22);
-  expect(notTriangle.checkType()).toEqual("not a triangle");
-});
-test('should correctly determine whether three lengths make a scalene tiangle', () =>
-{
-  const scalTriangle = new Triangle(4,5,7);
-  expect(scalTriangle.checkType()).toEqual("scalene triangle");
-});
-test('should correctly determine whether three lengths make an isocelese tringle', () => {
-  const isoscTriangle = new Triangle(5,5,7);
-  expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
-});
-test('should correctly determine Whether three lengths make an equilateral triangle', () => {
-  const equiTriangle = new Triangle(5,5,5);
-  expect(equiTriangle.checkType()).toEqual("equilateral triangle");
-});
+  });
+  test('should correctly determine whether three lengths are not a triangle', () => {
+    const notTriangle = new Triangle(3, 9, 22);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
+  test('should correctly determine whether three lengths make a scalene tiangle', () => {
+    const scalTriangle = new Triangle(4, 5, 7);
+    expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  });
+  test('should correctly determine whether three lengths make an isocelese tringle', () => {
+    const isoscTriangle = new Triangle(5, 5, 7);
+    expect(isoscTriangle.checkType()).toEqual("isosceles triangle");
+  });
+  test('should correctly determine Whether three lengths make an equilateral triangle', () => {
+    const equiTriangle = new Triangle(5, 5, 5);
+    expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  });
 });
 
 
